5d8632f50a8a1382a006daac47fc08c78d79bf77
Christian LaCourt <cllacour@mtu.edu>
2018-07-23 12:58:46 -0400

    Improved types and docs

--- SnipeSharp.PowerShell/Enums/AssetSort.cs
+++ SnipeSharp.PowerShell/Enums/AssetSort.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace SnipeSharp.PowerShell.Enums
 {
+    /// <summary>
+    /// Methods for the <see cref="SnipeSharp.PowerShell.Enums.AssetSort" /> type.
+    /// </summary>
     internal static class AssetSortMethods {
         private static Dictionary<AssetSort, string> _map = new Dictionary<AssetSort, string>{
             {AssetSort.Id, "id"},
@@ -27,30 +30,58 @@ namespace SnipeSharp.PowerShell.Enums
             {AssetSort.PurchaseCost, "purchase_cost"}
         };
 
+        /// <summary>
+        /// Converts an <see cref="SnipeSharp.PowerShell.Enums.AssetSort" /> into a string comptatible with the Snipe IT API.
+        /// </summary>
+        /// <param name="sort">A sort column.</param>
+        /// <returns>A Snipe IT API-compatible column string.</returns>
         public static string ToString(this AssetSort sort) => _map[sort];
     }
 
+    /// <summary>
+    /// Column on which to sort results in SnipeIT.
+    /// </summary>
     public enum AssetSort
     {
+        /// <summary>Sort assets by Snipe IT Internal Id.</summary>
         Id,
+        /// <summary>Sort assets by Name.</summary>
         Name,
+        /// <summary>Sort assets by Asset Tag.</summary>
         AssetTag,
+        /// <summary>Sort assets by Serial Id.</summary>
         Serial,
+        /// <summary>Sort assets by Model name.</summary>
         Model,
+        /// <summary>Sort assets by the Snipe IT internal Id of the Model.</summary>
         ModelNumber,
+        /// <summary>Sort assets by the the date they were last checked out.</summary>
         LastCheckout,
+        /// <summary>Sort assets by the name of the asset's category.</summary>
         Category,
+        /// <summary>Sort assets by the name of the Manufacturer.</summary>
         Manufacturer,
+        /// <summary>Sort assets by the contents of the Notes field.</summary>
         Notes,
+        /// <summary>Sort assets by the next expected check-in date.</summary>
         ExpectedCheckin,
+        /// <summary>Sort assets by the order number associated with their purchase.</summary>
         OrderNumber,
+        /// <summary>Sort assets by the associated owner company.</summary>
         CompanyName,
+        /// <summary>Sort assets by the name of the assigned location or assigned user's location.</summary>
         Location,
+        /// <summary>Sort assets by the URL of their picture.</summary>
         Image,
+        /// <summary>Sort assets by the name of the status label.</summary>
         StatusLabel,
+        /// <summary>Sort assets by the name of the assigned user.</summary>
         AssignedTo,
+        /// <summary>Sort assets by the date they were added to the system.</summary>
         CreatedAt,
+        /// <summary>Sort assets by the date they were purchased.</summary>
         PurchaseDate,
+        /// <summary>Sort assets by their cost.</summary>
         PurchaseCost
     }
 }
