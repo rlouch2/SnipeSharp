8bd04e0ff44dadce16fcb9b5c06a7baa59a5f27c
Christian LaCourt <cllacour@mtu.edu>
2018-07-26 12:37:48 -0400

    More New Cmdlets

--- SnipeSharp.PowerShell/Cmdlets/NewLicense.cs
+++ SnipeSharp.PowerShell/Cmdlets/NewLicense.cs
@@ -1,11 +1,103 @@
 ﻿using System;
 using System.Management.Automation;
+using SnipeSharp.PowerShell.BindingTypes;
+using SnipeSharp.PowerShell.Attributes;
+using SnipeSharp.Endpoints.Models;
+using SnipeSharp.Common;
 
 namespace SnipeSharp.PowerShell.Cmdlets
 {
     [Cmdlet(VerbsCommon.New, "License")]
     public class NewLicense: PSCmdlet
     {
-        // TODO
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipelineByPropertyName = true
+        )]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        [ValidateIdentityNotNull]
+        public CompanyIdentity Company { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string ExpirationDate { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public long? FreeSeatsCount { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string LicenseEmail { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string LicenseName { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public bool Maintained { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        [ValidateIdentityNotNull]
+        public ManufacturerIdentity Manufacturer { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string Notes { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string OrderNumber { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string ProductKey { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string PurchaseCost { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string PurchaseDate { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string PurchaseOrder { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public long Seats { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        [ValidateIdentityNotNull]
+        public CompanyIdentity Supplier { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public bool UserCanCheckout { get; set; }
+        
+        protected override void ProcessRecord()
+        {
+            var item = new License {
+                Name = this.Name,
+                Company = this.Company?.Company,
+                FreeSeatsCount = this.FreeSeatsCount,
+                LicenseEmail = this.LicenseEmail,
+                LicenseName = this.LicenseName,
+                Maintained = this.Maintained,
+                Manufacturer = this.Manufacturer?.Manufacturer,
+                Notes = this.Notes,
+                OrderNumber = this.OrderNumber,
+                ProductKey = this.ProductKey,
+                PurchaseCost = this.PurchaseCost,
+                PurchaseOrder = this.PurchaseOrder,
+                Seats = this.Seats,
+                Supplier = this.Supplier?.Company,
+                UserCanCheckout = this.UserCanCheckout
+            };
+            if(ExpirationDate != null)
+                item.ExpirationDate = new Date {
+                    DateObj = this.ExpirationDate
+                };
+            if(PurchaseDate != null)
+                item.PurchaseDate = new Date {
+                    DateObj = this.PurchaseDate
+                };
+            //TODO: error handling
+            WriteObject(ApiHelper.Instance.LicenseManager.Create(item).Payload);
+        }
     }
 }
--- SnipeSharp.PowerShell/Cmdlets/NewLocation.cs
+++ SnipeSharp.PowerShell/Cmdlets/NewLocation.cs
@@ -1,11 +1,59 @@
 ﻿using System;
 using System.Management.Automation;
+using SnipeSharp.Endpoints.Models;
+using SnipeSharp.PowerShell.Attributes;
+using SnipeSharp.PowerShell.BindingTypes;
 
 namespace SnipeSharp.PowerShell.Cmdlets
 {
     [Cmdlet(VerbsCommon.New, "Location")]
     public class NewLocation: PSCmdlet
     {
-        // TODO
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipelineByPropertyName = true
+        )]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string Address { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string City { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string State { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string Country { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string Zip { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        [ValidateIdentityNotNull]
+        public LocationIdentity Parent { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        [ValidateIdentityNotNull]
+        public UserIdentity Manager { get; set; }
+        
+        protected override void ProcessRecord()
+        {
+            var item = new Location {
+                Name = this.Name,
+                Address = this.Address,
+                City = this.City,
+                State = this.State,
+                Country = this.Country,
+                Zip = this.Zip,
+                Parent = this.Parent?.Location,
+                Manager = this.Manager?.User
+            };
+            //TODO: error handling
+            WriteObject(ApiHelper.Instance.LicenseManager.Create(item).Payload);
+        }
     }
 }
