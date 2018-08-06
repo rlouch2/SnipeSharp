66811d27d0b89718107fd60a81fcf6d6d9e14578
Christian LaCourt <cllacour@mtu.edu>
2018-08-06 12:46:19 -0400

    Update AssemblyInfo

--- SnipeSharp.Tests/Properties/AssemblyInfo.cs
+++ SnipeSharp.Tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SnipeSharp.Tests")]
-[assembly: AssemblyCopyright("Copyright ?  2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("012a8e24-aa43-47d1-9a5d-7f38f59da537")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
