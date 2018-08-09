f1c48502fe8f9444be6cb1b2898c3d876e9f5f2a
Christian LaCourt <cllacour@mtu.edu>
2018-08-09 15:49:29 -0400

    Convert to UTF8 w/o BOM

--- SnipeSharp.PowerShell/Cmdlets/FindManufacturer.cs
+++ SnipeSharp.PowerShell/Cmdlets/FindManufacturer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Management.Automation;
 using SnipeSharp.Endpoints.Models;
 using SnipeSharp.Endpoints.SearchFilters;
--- SnipeSharp.PowerShell/Cmdlets/SetAccessory.cs
+++ SnipeSharp.PowerShell/Cmdlets/SetAccessory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Management.Automation;
 using SnipeSharp.EndPoint.Models;
 using SnipeSharp.PowerShell.BindingTypes;
