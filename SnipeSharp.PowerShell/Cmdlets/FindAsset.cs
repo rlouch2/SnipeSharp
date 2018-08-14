835956c7b46f404d1742e69f89b8caaf3625c24c
Christian LaCourt <cllacour@mtu.edu>
2018-08-14 11:34:26 -0400

    Added stubs for all find cmdlets so you can filter those a little.

--- SnipeSharp.PowerShell/Cmdlets/FindAsset.cs
+++ SnipeSharp.PowerShell/Cmdlets/FindAsset.cs
@@ -1,86 +1,24 @@
 using System;
 using System.Management.Automation;
-using SnipeSharp.Endpoints.Models;
-using SnipeSharp.Endpoints.SearchFilters;
-using SnipeSharp.PowerShell.Enums;
-using SnipeSharp.PowerShell.Attributes;
+using SnipeSharp.EndPoint.Models;
+using SnipeSharp.EndPoint.Filters;
+using SnipeSharp.PowerShell.Cmdlets.AbstractCmdlets;
 using SnipeSharp.PowerShell.BindingTypes;
 
 namespace SnipeSharp.PowerShell.Cmdlets
 {
-    [Cmdlet(VerbsCommon.Find, "Asset",
+    [Cmdlet(VerbsCommon.Find, nameof(Asset),
         SupportsPaging = true
     )]
     [OutputType(typeof(Asset))]
-    public class FindAsset: PSCmdlet
+    public class FindAsset: FindObject<Asset, AssetSearchColumn, AssetSearchFilter>
     {
-        [Parameter(Position = 0, ValueFromPipeline = true)]
-        public string SearchString { get; set; }
-
-        [Parameter]
-        public Order SortOrder { get; set; }
-
-        [Parameter]
-        public SortColumn SortColumn { get; set; }
-
-        [Parameter]
-        public string OrderNumber { get; set; }
-
-        [Parameter]
-        [ValidateIdentityNotNull]
-        public ModelIdentity Model { get; set; }
-        
-        [Parameter]
-        [ValidateIdentityNotNull]
-        public CategoryIdentity Category { get; set; }
-
-        [Parameter]
-        [ValidateIdentityNotNull]
-        public CompanyIdentity Company { get; set; }
-
-        [Parameter]
-        [ValidateIdentityNotNull]
-        public LocationIdentity Location { get; set; }
-
-        [Parameter]
-        public string Status { get; set; }
-
-        [Parameter]
-        public StatusLabelIdentity StatusLabel { get; set; }
+        // TODO: parameters
 
         /// <inheritdoc />
-        protected override void ProcessRecord()
+        protected override void PopulateFilter(AssetSearchFilter filter)
         {
-            var filter = new AssetSearchFilter();
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(SearchString)))
-                filter.Search = SearchString;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(SortOrder)))
-                filter.Order = SortOrder.ToApiString();
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(SortColumn)))
-                filter.Sort = SortColumn.ToApiString();
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(OrderNumber)))
-                filter.OrderNumber = OrderNumber;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(Model)))
-                filter.ModelId = (int) Model.Model.Id;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(Category)))
-                filter.CategoryId = (int) Category.Category.Id;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(Company)))
-                filter.CompanyId = (int) Company.Company.Id;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(Location)))
-                filter.Location = Location.Location;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(Status)))
-                filter.Status = Status;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(StatusLabel)))
-                filter.StatusId = (int) StatusLabel.StatusLabel.Id;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(PagingParameters.First)))
-                filter.Limit = (int) PagingParameters.First;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(PagingParameters.Skip)))
-                filter.Offset = (int) PagingParameters.Skip;
-            var results = ApiHelper.Instance.AssetManager.FindAll(filter);
-            // TODO: error handling
-            if(PagingParameters.IncludeTotalCount)
-                WriteObject(results.Total);
-            WriteObject(results.Rows, true);
+            //TODO: parameters
         }
     }
 }
--- SnipeSharp.PowerShell/Cmdlets/FindCompany.cs
+++ SnipeSharp.PowerShell/Cmdlets/FindCompany.cs
@@ -1,45 +1,21 @@
 using System;
 using System.Management.Automation;
-using SnipeSharp.Endpoints.Models;
-using SnipeSharp.Endpoints.SearchFilters;
-using SnipeSharp.PowerShell.Enums;
+using SnipeSharp.EndPoint.Models;
+using SnipeSharp.EndPoint.Filters;
+using SnipeSharp.PowerShell.Cmdlets.AbstractCmdlets;
 
 namespace SnipeSharp.PowerShell.Cmdlets
 {
-    [Cmdlet(VerbsCommon.Find, "Company",
+    [Cmdlet(VerbsCommon.Find, nameof(Company),
         SupportsPaging = true
     )]
     [OutputType(typeof(Company))]
-    public class FindCompany: PSCmdlet
+    public class FindCompany: FindObject<Company, string, SearchFilter>
     {
-        [Parameter(Position = 0, ValueFromPipeline = true)]
-        public string SearchString { get; set; }
-
-        [Parameter]
-        public Order SortOrder { get; set; }
-
-        [Parameter]
-        public SortColumn SortColumn { get; set; }
-
         /// <inheritdoc />
-        protected override void ProcessRecord()
+        protected override void PopulateFilter(SearchFilter filter)
         {
-            var filter = new SearchFilter();
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(SearchString)))
-                filter.Search = SearchString;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(SortOrder)))
-                filter.Order = SortOrder.ToApiString();
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(SortColumn)))
-                filter.Sort = SortColumn.ToApiString();
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(PagingParameters.First)))
-                filter.Limit = (int) PagingParameters.First;
-            if(MyInvocation.BoundParameters.ContainsKey(nameof(PagingParameters.Skip)))
-                filter.Offset = (int) PagingParameters.Skip;
-            var results = ApiHelper.Instance.CompanyManager.FindAll(filter);
-            // TODO: error handling
-            if(PagingParameters.IncludeTotalCount)
-                WriteObject(results.Total);
-            WriteObject(results.Rows, true);
+            // nop
         }
     }
 }
