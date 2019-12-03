1fbc03d801cbddcfccef3b3e04a44212bbd8f1f0
Christian LaCourt <cllacour@mtu.edu>
2019-12-03 13:39:08 -0500

    Start implementing accessory checkout

--- SnipeSharp/EndPoint/AccessoryEndPoint.cs
+++ SnipeSharp/EndPoint/AccessoryEndPoint.cs
@@ -18,5 +18,25 @@ namespace SnipeSharp.EndPoint
         /// <returns>A ResponseCollection list of AccessoryCheckOuts.</returns>
         public ResponseCollection<AccessoryCheckOut> GetCheckedOut(Accessory accessory)
             => Api.RequestManager.GetAll<AccessoryCheckOut>($"{EndPointInfo.BaseUri}/{accessory.Id}/checkedout").RethrowExceptionIfAny().Value;
+
+        /// <summary>
+        /// Check out an accessory.
+        /// </summary>
+        /// <param name="accessory">The accessory to check out.</param>
+        /// <param name="assignedUser">The user to check out to.</param>
+        /// <param name="note">An optional note for the checkout log.</param>
+        /// <returns></returns>
+        public RequestResponse<ApiObject> CheckOut(Accessory accessory, User assignedUser, string note = null)
+            => CheckOut(new AccessoryCheckOutRequest(accessory, assignedUser){ Note = note });
+
+        /// <summary>
+        /// Check out an accessory.
+        /// </summary>
+        /// <param name="request">An accessory check-out request.</param>
+        /// <returns></returns>
+        public RequestResponse<ApiObject> CheckOut(AccessoryCheckOutRequest request)
+            => Api.RequestManager.Post<AccessoryCheckOutRequest, ApiObject>($"{EndPointInfo.BaseUri}/{request.Accessory.Id}/checkout", request).RethrowExceptionIfAny().Value;
+
+        // TODO: checkin
     }
 }
---
+++ SnipeSharp/Models/AccessoryCheckOutRequest.cs
@@ -0,0 +1,34 @@
+using SnipeSharp.Serialization;
+using static SnipeSharp.Serialization.FieldConverter;
+
+namespace SnipeSharp.Models
+{
+    /// <summary>
+    /// A request to associate an Accessory with a User.
+    /// </summary>
+    public sealed class AccessoryCheckOutRequest : ApiObject
+    {
+        /// <value>The Accessory that will be checkout out.</value>
+        /// <remarks>This property is not serialized, but instead used for its Id value.</remarks>
+        public Accessory Accessory { get; private set; }
+
+        /// <value>The assigned user.</value>
+        [Field("assigned_to", Converter = CommonModelConverter)]
+        public User AssignedUser { get; private set; }
+
+        /// <value>The note to put in the log for this check-out event.</value>
+        [Field("note")]
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Begins a new AccessoryCheckOutRequest assigning the supplied accessory to the supplied user.
+        /// </summary>
+        /// <param name="accessory">The accessory to assign.</param>
+        /// <param name="user">The User to assign the accessory to.</param>
+        public AccessoryCheckOutRequest(Accessory accessory, User user)
+        {
+            Accessory = accessory;
+            AssignedUser = user;
+        }
+    }
+}
