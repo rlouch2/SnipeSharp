e199f60a498d8cb1ed04ec4a670626dfe454c8cc
Christian LaCourt <cllacour@mtu.edu>
2019-12-03 13:06:44 -0500

    Add license seat filter PowerShell support

--- SnipeSharp.PowerShell/Cmdlets/Get/GetLicenseSeat.cs
+++ SnipeSharp.PowerShell/Cmdlets/Get/GetLicenseSeat.cs
@@ -1,11 +1,15 @@
 using System.Management.Automation;
+using SnipeSharp.Filters;
 using SnipeSharp.Models;
 using SnipeSharp.PowerShell.BindingTypes;
 
 namespace SnipeSharp.PowerShell.Cmdlets
 {
     /// <summary>Gets the License Seats of a Snipe IT License.</summary>
-    /// <remarks>The Get-LicenseSeat cmdlet gets, for each license provided, the license seat objects associated with that license.</remarks>
+    /// <remarks>
+    ///   <p>The Get-LicenseSeat cmdlet gets, for each license provided, the license seat objects associated with that license.</p>
+    ///   <p>Paging is per-license, not overall. If you need to process a certain number of seats from multiple licenses, use <see>Select-Object</see>.</p>
+    /// </remarks>
     /// <example>
     ///   <code>Get-LicenseSeat License12</code>
     ///   <para>Retrieves the license seats for the license License12.</para>
@@ -14,13 +18,21 @@ namespace SnipeSharp.PowerShell.Cmdlets
     ///   <code>Get-LicenseSeat License12, License11</code>
     ///   <para>Retrieves the license seats for the licenses License12 and License11.</para>
     /// </example>
-    [Cmdlet(VerbsCommon.Get, "LicenseSeat")]
+    [Cmdlet(VerbsCommon.Get, "LicenseSeat", SupportsPaging = true)]
     [OutputType(typeof(LicenseSeat))]
     public sealed class GetLicenseSeat: BaseCmdlet
     {
         /// <summary>The license to retrieve the seats of.</summary>
         [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
         public ObjectBinding<License>[] License { get; set; }
+
+        /// <summary>Which way to sort the data.</summary>
+        [Parameter]
+        public SearchOrder SortOrder { get; set; }
+
+        /// <summary>On which column to sort the data.</summary>
+        [Parameter]
+        public LicenseSeatSearchColumn SortColumn { get; set; }
 
         /// <summary>If present, return the result as a <see cref="SnipeSharp.Models.ResponseCollection{T}"/> rather than enumerating.</summary>
         [Parameter]
@@ -29,10 +41,19 @@ namespace SnipeSharp.PowerShell.Cmdlets
         /// <inheritdoc />
         protected override void ProcessRecord()
         {
+            var filter = new LicenseSeatSearchFilter();
+            if(MyInvocation.BoundParameters.ContainsKey(nameof(SortOrder)))
+                filter.Order = SortOrder;
+            if(MyInvocation.BoundParameters.ContainsKey(nameof(SortColumn)))
+                filter.SortColumn = SortColumn;
+            if(MyInvocation.BoundParameters.ContainsKey(nameof(PagingParameters.First)))
+                filter.Limit = (int) PagingParameters.First;
+            if(MyInvocation.BoundParameters.ContainsKey(nameof(PagingParameters.Skip)))
+                filter.Offset = (int) PagingParameters.Skip;
             foreach(var item in License)
             {
                 if (GetSingleValue(item, out var itemValue))
-                    WriteObject(ApiHelper.Instance.Licenses.GetSeats(itemValue), !NoEnumerate.IsPresent);
+                    WriteObject(ApiHelper.Instance.Licenses.GetSeats(itemValue, filter), !NoEnumerate.IsPresent);
             }
         }
     }
